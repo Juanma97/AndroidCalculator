ace91984fdfefe441d740fd7a6ea97dc1dea6111
Juan Manuel Perez Toro <juanma97@outlook.com>
2020-06-14 15:03:11 +0100

    Added library to evaluate expression

--- app/src/main/java/com/juanmaperez/calculator/MainActivity.kt
+++ app/src/main/java/com/juanmaperez/calculator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.juanmaperez.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -41,9 +42,33 @@ class MainActivity : AppCompatActivity() {
             }
             textViewResult.text = ""
         }
+
+        textViewEquals.setOnClickListener {
+            try{
+
+                val expression = ExpressionBuilder(textViewExpression.text.toString()).build()
+                val result = expression.evaluate()
+                val longResult = result.toLong()
+
+                if(result == longResult.toDouble()){
+                    textViewResult.text = longResult.toString()
+                }else{
+                    textViewResult.text = result.toString()
+                }
+
+
+            }catch (e: Exception){
+
+            }
+        }
     }
 
     fun appendOnExpression(string: String, canClear: Boolean) {
+
+        if(textViewResult.text.isNotEmpty()){
+            textViewExpression.text = ""
+        }
+
         if (canClear) {
             textViewResult.text = ""
             textViewExpression.append(string)
